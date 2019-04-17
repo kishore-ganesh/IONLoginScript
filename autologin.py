835332755a456698674f2a406e5d10b2ecb788b2
kalpaj12 <kalpaj.agrawalla01@gmail.com>
2019-04-17 23:43:32 +0530

    ION login improv

--- autologin.py
+++ autologin.py
@@ -1,28 +1,35 @@
 import subprocess, requests
 
-output=subprocess.Popen(["ip", "addr", "show", "wlp2s0"], stdout=subprocess.PIPE)
-output=output.communicate()[0];
-output=str(output)
-print("HELLO" + output)
-ind1=output.find("inet")
+ip_route=subprocess.Popen(["ip", "route"], stdout=subprocess.PIPE)
+ip_route=ip_route.communicate()[0]
+ip_route=str(ip_route)
 
-output=output[ind1:]
+default_pos_ip_route = ip_route.find('via ')
+default =  ip_route[(default_pos_ip_route+4):]
+default = default.split(' dev')[0]
+default = str(default)
 
-ind2=output.find(" brd")
-output=output[5:ind2]
-output=output.split('/')[0]
-output=output.split(".")
-output[len(output)-1]="1";
-output[len(output)-2]="0"
-s="."
-s=s.join(output)
-url="http://"+s+"/"+"login"
+my_ip_pos = ip_route.find('src ')
+my_ip = ip_route[(my_ip_pos+4):]
+my_ip = my_ip.split(' metric')[0]
+my_ip = str(my_ip)
 
-r=requests.post(url, {
+if default:
+	print("Gateway: " + default)
+	print("local ip: " + my_ip)
 
-    'username': '',
-    'password': '',
-    'radius1-44115'	:'12'
-})
+	try:
+		url="http://"+default+"/"+"login"
+		r=requests.post(url, {
+			'username': '',
+			'password': '',
+			'radius1-44115'	:'12'
+		},timeout=(1,1))
 
-print(r.text)
+		if r.text.find("You are logged in"):
+				print("Log in success!")
+	except:
+			print("=>Time-out<=")
+
+else:
+	print("=>Bad Gateway<=")
